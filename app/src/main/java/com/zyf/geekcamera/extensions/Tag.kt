eb370309265d2ba21b43c32181c708224f30cf51
zhaoyuanfa <2925261914@qq.com>
2023-06-10 03:13:20 +0800

    new project

---
+++ app/src/main/java/com/zyf/geekcamera/extensions/Tag.kt
@@ -0,0 +1,3 @@
+package com.zyf.geekcamera.extensions
+
+fun Any.TAG(): String = this::class.java.name
---
+++ app/src/main/java/com/zyf/geekcamera/ui/GeekCamera.kt
@@ -0,0 +1,9 @@
+package com.zyf.geekcamera.ui
+
+import android.app.Application
+
+class GeekCamera: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
